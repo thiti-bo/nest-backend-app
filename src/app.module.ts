0e4876bf6aba7abefd8b7cb607f2b388dedf0f95
StarLord <thiti_booncharoen@hotmail.com>
2021-07-24 14:51:34 +0700

    add main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepartmentModule } from './department/department.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [],
+  imports: [DepartmentModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
